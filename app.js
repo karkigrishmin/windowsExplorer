67aedfe5c4f027a5598c7e0e58d4f2cb3a54a93e
Grishmin karki <grishmink@gmail.com>
2020-04-17 00:35:17 +0545

    js

--- app.js
+++ app.js
@@ -6,14 +6,34 @@ document.querySelector(".menu-1").addEventListener("click", function (e) {
   if (list.style.display === "block") {
     list.style.display = "none";
 
-    document.querySelector(".menu-1").innerHTML = `
+    document.querySelector(".menu-icon").innerHTML = `
     <i class="fas fa-angle-down icon-light"></i>
     `;
+    // const item = document.querySelector(".updated-margin");
+    // item.style.postition = "relative";
+    // item.style.bottom = "-20rem";
   } else {
     list.style.display = "block";
-    document.querySelector(".menu-1").innerHTML = `
+    document.querySelector(".menu-icon").innerHTML = `
     <i class="fas fa-angle-up icon-light"></i>
     `;
   }
   e.preventDefault();
 });
+
+document.querySelector(".menu-2").addEventListener("click", function (e) {
+  //   const ul=document.createElement('ul');
+  //   ul.className='collection';
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode("Item-1"));
+  document.querySelector(".item").appendChild(li);
+
+  e.preventDefault();
+});
+
+// for image
+const property = [
+  {
+    image,
+  },
+];
