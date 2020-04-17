514085af53b46ac9fe8f82120bc74ef4ab131a48
Grishmin karki <grishmink@gmail.com>
2020-04-17 20:25:48 +0545

    js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+//for this pc menu
 document.querySelector(".menu-1").addEventListener("click", function (e) {
-  const lists = document.querySelectorAll(".list-item");
+  const lists = document.querySelectorAll(".collection-1");
 
   lists.forEach((list) => {
     if (list.style.display === "block") {
@@ -11,6 +12,48 @@ document.querySelector(".menu-1").addEventListener("click", function (e) {
     } else {
       list.style.display = "block";
       document.querySelector(".menu-icon").innerHTML = `
+    <i class="fas fa-angle-up icon-light"></i>
+    `;
+    }
+  });
+
+  e.preventDefault();
+});
+
+//for Quick Access
+document.querySelector(".menu-2").addEventListener("click", function (e) {
+  const lists = document.querySelectorAll(".collection-2");
+
+  lists.forEach((list) => {
+    if (list.style.display === "block") {
+      list.style.display = "none";
+
+      document.querySelector(".menu-icon-2").innerHTML = `
+    <i class="fas fa-angle-down icon-light"></i>
+    `;
+    } else {
+      list.style.display = "block";
+      document.querySelector(".menu-icon-2").innerHTML = `
+    <i class="fas fa-angle-up icon-light"></i>
+    `;
+    }
+  });
+
+  e.preventDefault();
+});
+document.querySelector(".menu-3").addEventListener("click", function (e) {
+  const lists = document.querySelectorAll(".collection-3");
+
+  lists.forEach((list) => {
+    if (list.style.display === "block") {
+      list.style.display = "none";
+
+      document.querySelector(".menu-icon-3").innerHTML = `
+    <i class="fas fa-angle-down icon-light"></i>
+    `;
+    } else {
+      list.style.display = "block";
+      document.querySelector(".menu-icon-3").innerHTML = `
     <i class="fas fa-angle-up icon-light"></i>
     `;
     }
