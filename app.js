55bc8a5dbccd3b778ba545b477633ed2fb5a5068
Grishmin karki <grishmink@gmail.com>
2020-04-17 18:02:43 +0545

    js

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ document.querySelector(".menu-1").addEventListener("click", function (e) {
       document.querySelector(".menu-icon").innerHTML = `
     <i class="fas fa-angle-down icon-light"></i>
     `;
-      // const item = document.querySelector(".updated-margin");
-      // item.style.postition = "relative";
-      // item.style.bottom = "-20rem";
     } else {
       list.style.display = "block";
       document.querySelector(".menu-icon").innerHTML = `
@@ -18,54 +15,6 @@ document.querySelector(".menu-1").addEventListener("click", function (e) {
     `;
     }
   });
-  // if (list.style.display === "block") {
-  //   list.style.display = "none";
 
-  //   document.querySelector(".menu-icon").innerHTML = `
-  //   <i class="fas fa-angle-down icon-light"></i>
-  //   `;
-  //   // const item = document.querySelector(".updated-margin");
-  //   // item.style.postition = "relative";
-  //   // item.style.bottom = "-20rem";
-  // } else {
-  //   list.style.display = "block";
-  //   document.querySelector(".menu-icon").innerHTML = `
-  //   <i class="fas fa-angle-up icon-light"></i>
-  //   `;
-  // }
   e.preventDefault();
 });
-
-// document.querySelector(".menu-2").addEventListener("click", function (e) {
-//   //   const ul=document.createElement('ul');
-//   //   ul.className='collection';
-//   const li = document.createElement("li");
-//   li.appendChild(document.createTextNode("Item-1"));
-//   document.querySelector(".item").appendChild(li);
-
-//   e.preventDefault();
-// });
-
-// const imgArrs = [
-//   {
-//     source:
-//       "https://rukminim1.flixcart.com/image/832/832/shoe/b/6/c/red-basic-low-fila-3-original-imadgyh7swgmych8.jpeg?q=70",
-//     title: "Nike",
-//   },
-//   {
-//     source:
-//       "https://rukminim1.flixcart.com/image/832/832/shoe/b/6/c/red-basic-low-fila-3-original-imadgyh7swgmych8.jpeg?q=70/red-basic-low-fila-3-original-imadgyh7swgmych8.jpeg.webp",
-//     title: "Nike",
-//   },
-// ];
-
-// // foreach loop
-// imgArrs.forEach((img) => {
-//   const container = document.createElement("div");
-//   container.className = "img-grid";
-//   const image = document.createElement("img");
-//   image.src = im;
-//   container.appendChild(image);
-//   // const title = document.createElement(p);
-//   document.querySelector(".img-grid").appendChild(container);
-// });
